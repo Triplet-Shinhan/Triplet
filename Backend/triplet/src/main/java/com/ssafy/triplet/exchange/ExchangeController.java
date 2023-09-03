b7ff85df826b12bcd92055dd3900aefc1afad7d4
takealook <takealook97@naver.com>
2023-09-03 15:27:19 +0900

    feat: ExchangeController 생성

---
+++ Backend/triplet/src/main/java/com/ssafy/triplet/exchange/ExchangeController.java
@@ -0,0 +1,10 @@
+package com.ssafy.triplet.exchange;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/exchanges")
+public class ExchangeController {
+
+}
