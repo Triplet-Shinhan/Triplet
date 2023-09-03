32a23e3c604c5ffce7f728c649327af4f1fb49e9
fangdol888 <fangdol888@gmail.com>
2023-09-04 00:10:57 +0900

    feat : getBranchLocation 메소드 추가

--- Backend/triplet/src/main/java/com/ssafy/triplet/exchange/ExchangeController.java
+++ Backend/triplet/src/main/java/com/ssafy/triplet/exchange/ExchangeController.java
@@ -1,22 +1,31 @@
 package com.ssafy.triplet.exchange;
 
-import com.ssafy.triplet.exchange.dto.ExchangeDto;
-import com.ssafy.triplet.exchange.service.ExchangeService;
-import org.springframework.http.ResponseEntity;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.ssafy.triplet.exchange.dto.ExchangeDto;
+import com.ssafy.triplet.exchange.dto.NearBranchDto;
+import com.ssafy.triplet.exchange.dto.NearBranchRequestDto;
+import com.ssafy.triplet.exchange.service.ExchangeService;
 
 @RestController
 @RequestMapping("/api/exchanges")
 public class ExchangeController {
 
     ExchangeService exchangeService;
-    @GetMapping("")
-    public ExchangeDto getExchange(@RequestParam("currency") String currency ){
 
+    @GetMapping("")
+    public ExchangeDto getExchange(@RequestParam("currency") String currency) {
         return exchangeService.getRate(currency);
 
+    }
+
+    @GetMapping("/locations")
+    public NearBranchDto getBranchLocation(NearBranchRequestDto nearBranchRequest) {
+        return exchangeService.getBranch(nearBranchRequest.getLatitude(), nearBranchRequest.getLongtitude());
     }
 }
