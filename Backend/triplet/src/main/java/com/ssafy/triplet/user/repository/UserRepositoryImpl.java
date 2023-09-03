fcae49b196023ddf5c827e1f08f3d2695b77a774
takealook <takealook97@naver.com>
2023-09-03 21:29:40 +0900

    feat: jdbcTemplate생성 및 userId기반으로 가입여부 판단 메서드 추가
    body: userId를 기반으로 쿼리를 통해 이미 가입된 userId인지 판단하고 이를 기반으로 boolean값을 리턴한다. 결과가 false이거나 db에서 못찾을 경우 예외를 던진다.

---
+++ Backend/triplet/src/main/java/com/ssafy/triplet/user/repository/UserRepositoryImpl.java
@@ -0,0 +1,58 @@
+package com.ssafy.triplet.user.repository;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import com.ssafy.triplet.exception.BaseException;
+import com.ssafy.triplet.exception.ErrorCode;
+import com.ssafy.triplet.user.domain.User;
+
+import jakarta.transaction.Transactional;
+
+@Repository
+public class UserRepositoryImpl implements UserRepository {
+
+	private final NamedParameterJdbcTemplate jdbcTemplate;
+
+	@Autowired
+	public UserRepositoryImpl(NamedParameterJdbcTemplate jdbcTemplate) {
+		this.jdbcTemplate = jdbcTemplate;
+	}
+
+	@Transactional
+	@Override
+	public Optional<User> save(final User user) {
+
+	}
+
+	private boolean isExistUserByUserId(final Long userId) {
+		try {
+			return Boolean.TRUE.equals(jdbcTemplate.queryForObject(
+				"SELECT EXISTS (SELECT user_id FROM users WHERE user_id = :userId)",
+				Map.of("userId", userId), Boolean.class));
+		} catch (final EmptyResultDataAccessException e) {
+			throw new BaseException(ErrorCode.EMAIL_NOT_FOUND);
+		}
+	}
+
+	@Override
+	public List<User> findAll() {
+
+	}
+
+	@Override
+	public Optional<User> findByUserId(Long userId) {
+
+	}
+
+	@Override
+	public void update(User user) {
+
+	}
+}
