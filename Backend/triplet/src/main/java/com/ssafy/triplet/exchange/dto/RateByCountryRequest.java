e1ff08fee91ef5391dc4e729789339bd3a0653d4
fangdol888 <fangdol888@gmail.com>
2023-09-03 21:39:07 +0900

    feat: 환율 조회 request 클래스 추가

---
+++ Backend/triplet/src/main/java/com/ssafy/triplet/exchange/dto/RateByCountryRequest.java
@@ -0,0 +1,30 @@
+package com.ssafy.triplet.exchange.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@RequiredArgsConstructor
+public class RateByCountryRequest {
+    private final DataHeader dataHeader;
+    private final DataBody DataBody;
+
+    public RateByCountryRequest(String apikey, String serviceCode) {
+        dataHeader = new DataHeader(apikey);
+        DataBody = new DataBody(serviceCode);
+    }
+
+    @Data
+    public static class DataHeader {
+        private final String apikey;
+    }
+
+    @Data
+    public static class DataBody {
+        private final String serviceCode;
+    }
+}
