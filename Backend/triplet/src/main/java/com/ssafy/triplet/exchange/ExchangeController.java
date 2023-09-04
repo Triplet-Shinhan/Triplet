6437b81021db489a317d3f47c474aeb7a734517b
fangdol888 <fangdol888@gmail.com>
2023-09-04 22:07:05 +0900

    add : 환전 컨트롤러 기본 틀 생성

--- Backend/triplet/src/main/java/com/ssafy/triplet/exchange/ExchangeController.java
+++ Backend/triplet/src/main/java/com/ssafy/triplet/exchange/ExchangeController.java
@@ -1,15 +1,19 @@
 package com.ssafy.triplet.exchange;
 
-import java.util.List;
-
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.ssafy.triplet.exchange.dto.ExchangeDto;
-import com.ssafy.triplet.exchange.dto.NearBranchDto;
+import com.ssafy.triplet.exchange.dto.ExchangeApplyRequestDto;
+import com.ssafy.triplet.exchange.dto.ExchangeApplyResponseDto;
+import com.ssafy.triplet.exchange.dto.ExchangeResponseDto;
+import com.ssafy.triplet.exchange.dto.ExchangeResultsRequestDto;
+import com.ssafy.triplet.exchange.dto.ExchangeResultsResponseDto;
 import com.ssafy.triplet.exchange.dto.NearBranchRequestDto;
+import com.ssafy.triplet.exchange.dto.NearBranchResponseDto;
 import com.ssafy.triplet.exchange.service.ExchangeService;
 
 @RestController
@@ -19,13 +23,24 @@ public class ExchangeController {
     ExchangeService exchangeService;
 
     @GetMapping("")
-    public ExchangeDto getExchange(@RequestParam("currency") String currency) {
+    public ExchangeResponseDto getExchange(@RequestParam("currency") String currency) {
         return exchangeService.getRate(currency);
 
     }
 
     @GetMapping("/locations")
-    public NearBranchDto getBranchLocation(NearBranchRequestDto nearBranchRequest) {
-        return exchangeService.getBranch(nearBranchRequest.getLatitude(), nearBranchRequest.getLongtitude());
+    public NearBranchResponseDto getBranchLocation(NearBranchRequestDto nearBranchRequest) {
+        return exchangeService.getNearBranch(nearBranchRequest.getLatitude(), nearBranchRequest.getLongitude());
+    }
+
+    @PostMapping("")
+    public ExchangeApplyResponseDto applyExchange(@RequestBody ExchangeApplyRequestDto ExchangeApplyRequestDto) {
+        return exchangeService.applyExchange(ExchangeApplyRequestDto);
+    }
+
+    @GetMapping("/results")
+    public ExchangeResultsResponseDto getExchangeResults(
+            @RequestBody ExchangeResultsRequestDto exchangeResultsRequestDto) {
+        return exchangeService.getExchangeResults(exchangeResultsRequestDto);
     }
 }
