8786c82156e8d38eda46f0b540ef12fa54333fe1
fangdol888 <fangdol888@gmail.com>
2023-09-04 20:17:03 +0900

    add :  환전 서비스 클래스에 getNearBranch 기본 틀 추가

--- Backend/triplet/src/main/java/com/ssafy/triplet/exchange/service/ExchangeService.java
+++ Backend/triplet/src/main/java/com/ssafy/triplet/exchange/service/ExchangeService.java
@@ -1,20 +1,26 @@
 package com.ssafy.triplet.exchange.service;
 
 import com.ssafy.triplet.exchange.dto.ExchangeDto;
+import com.ssafy.triplet.exchange.dto.NearBranchDto;
+
 import org.springframework.stereotype.Service;
 
 @Service
 public class ExchangeService {
 
-    public ExchangeDto getRate(String currency){
+    public ExchangeDto getRate(String currency) {
 
-        //신한 api 요청 해서 통화코드, 환율 , 환전신청단위 , 우대율
+        // 신한 api 요청 해서 통화코드, 환율 , 환전신청단위 , 우대율
 
-//
-//        return  ExchangeDto.builder()
-//                .build()
-//                .setRate(181.58f)
+        //
+        // return ExchangeDto.builder()
+        // .build()
+        // .setRate(181.58f)
 
-    return new ExchangeDto();
+        return new ExchangeDto();
+    }
+
+    public NearBranchDto getNearBranch(Long latitude, Long longtitude) {
+        return new NearBranchDto();
     }
 }
