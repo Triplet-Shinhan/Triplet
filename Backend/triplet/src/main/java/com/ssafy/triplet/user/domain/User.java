3bfa6f8ebbb511aa307d801f6cb7ca41e36622d5
takealook <takealook97@naver.com>
2023-09-04 23:00:23 +0900

    fix: User 도메인 수정 및 Repository Impl 클래스 삭제

--- Backend/triplet/src/main/java/com/ssafy/triplet/user/domain/User.java
+++ Backend/triplet/src/main/java/com/ssafy/triplet/user/domain/User.java
@@ -1,8 +1,13 @@
 package com.ssafy.triplet.user.domain;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
@@ -10,15 +15,25 @@ import lombok.Setter;
 @Entity
 @Getter
 @Setter
+@Builder
+@Table(name = "users")
 @AllArgsConstructor
 @RequiredArgsConstructor
 public class User {
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY) //MySQL의 AUTO_INCREMENT를 사용
 	private Long userId;
+
+	@Column(name = "name")
 	private String name;
+	@Column(name = "birth")
 	private String birth;
+	@Column(name = "email")
 	private String email;
+	@Column(name = "password")
 	private String password;
+	@Column(name = "phoneNum")
 	private String phoneNum;
+	@Column(name = "accountNum")
 	private String accountNum;
 }
