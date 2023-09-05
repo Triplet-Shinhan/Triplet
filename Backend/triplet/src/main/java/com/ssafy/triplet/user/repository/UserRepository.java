2477e74ed2d3326ebff3f98d8d491e76202e8d11
takealook <takealook97@naver.com>
2023-09-06 03:02:43 +0900

    feat: 로그인 시 활용할 findByEmail메서드 추가

--- Backend/triplet/src/main/java/com/ssafy/triplet/user/repository/UserRepository.java
+++ Backend/triplet/src/main/java/com/ssafy/triplet/user/repository/UserRepository.java
@@ -1,5 +1,7 @@
 package com.ssafy.triplet.user.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,4 +9,5 @@ import com.ssafy.triplet.user.domain.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+	Optional<User> findByEmail(String email);
 }
