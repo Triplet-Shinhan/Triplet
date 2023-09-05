a62c4c6569fa830b68d3e51a6ce728d96cf4d310
yeongjin <hanyj0104@naver.com>
2023-09-05 21:08:07 +0900

    feat: 환전신청 api 메소드 생성

--- Backend/triplet/src/main/java/com/ssafy/triplet/parser/WebClientUtil.java
+++ Backend/triplet/src/main/java/com/ssafy/triplet/parser/WebClientUtil.java
@@ -2,6 +2,10 @@ package com.ssafy.triplet.parser;
 
 import com.ssafy.triplet.parser.dto.DataBodyRequest;
 import com.ssafy.triplet.parser.dto.DataHeaderRequest;
+import com.ssafy.triplet.parser.dto.exchange.Exchange;
+import com.ssafy.triplet.parser.dto.exchange.ExchangeDataBody;
+import com.ssafy.triplet.parser.dto.exchange.ExchangeReqDto;
+import com.ssafy.triplet.parser.dto.exchange.ExchangeResDto;
 import com.ssafy.triplet.parser.dto.rateParser.CurrencyRate;
 import com.ssafy.triplet.parser.dto.rateParser.RateReqDto;
 import com.ssafy.triplet.parser.dto.rateParser.RateResDto;
@@ -27,9 +31,10 @@ public class WebClientUtil {
     public List<CurrencyRate> getPreferentialRate() {
         String url = "https://shbhack.shinhan.com/v1/search/fx/discount-rate";
 
+        //헤더 설정
         DataHeaderRequest dataHeader = new DataHeaderRequest();
         dataHeader.setApikey(apiKey);
-
+        //요청body 설정
         DataBodyRequest dataBody = new DataBodyRequest();
         dataBody.setServiceCode("T0501");
 
@@ -37,6 +42,7 @@ public class WebClientUtil {
         RateReqDto rateReqDto = new RateReqDto();
         rateReqDto.setDataHeader(dataHeader);
         rateReqDto.setDataBody(dataBody);
+
         RateResDto rateResDto=webClient.post()
                 .uri(url)
                 .header("Content-Type", "application/json; charset=UTF-8")
@@ -49,5 +55,35 @@ public class WebClientUtil {
 
 
         return rateResDto.getRateDataBody().getCurrencyRate();
+    }
+
+
+
+    //환전신청 api
+    public ExchangeDataBody createExchange(Exchange exchange){
+        String url = "https://shbhack.shinhan.com/v1/request/fx";
+        //헤더 설정
+        DataHeaderRequest dataHeader = new DataHeaderRequest();
+        dataHeader.setApikey(apiKey);
+        //요청body 설정
+        exchange.setServiceCode("T0511");
+
+        // API 요청 객체 설정
+        ExchangeReqDto exchangeReqDto = new ExchangeReqDto();
+        exchangeReqDto.setDataHeader(dataHeader);
+        exchangeReqDto.setDataBody(exchange);
+
+        ExchangeResDto exchangeResDto=webClient.post()
+                .uri(url)
+                .header("Content-Type", "application/json; charset=UTF-8")
+                .bodyValue(exchangeReqDto)
+                .retrieve()
+                .bodyToMono(ExchangeResDto.class)
+                .block();
+
+        //에러처리
+
+        return exchangeResDto.getExchangeDataBody();
+
     }
 }
