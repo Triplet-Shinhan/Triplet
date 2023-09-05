01e327bf25005b6444db35699d75999ede333f5b
Lainlnya <kimyelim100@gmail.com>
2023-09-05 23:39:58 +0900

    feat: add api & context

---
+++ Frontend/triplet/src/api/BankAccount.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+import React from 'react';
+
+export default class BankAccount {
+  constructor() {
+    this.httpClient = axios.create({
+      baseURL: 'https:/localhost:8080',
+    });
+  }
+
+  // 계좌번호 인증 (본인계좌여야 가능하기 때문에 name, account)
+  async verifyBankAccount(name, account) {
+    return this.httpClient
+      .post('', {
+        params: {},
+      })
+      .then((res) => res.data.items);
+  }
+}
