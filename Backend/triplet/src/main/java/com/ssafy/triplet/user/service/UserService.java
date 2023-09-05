1dba34f338c15754e6b7beae6228f0e5412df187
takealook <takealook97@naver.com>
2023-09-06 01:44:19 +0900

    feat: User Service 생성 및 회원가입 메서드 구현

--- Backend/triplet/src/main/java/com/ssafy/triplet/user/service/UserService.java
+++ Backend/triplet/src/main/java/com/ssafy/triplet/user/service/UserService.java
@@ -2,7 +2,19 @@ package com.ssafy.triplet.user.service;
 
 import org.springframework.stereotype.Service;
 
-@Service
-public class UserService {
+import com.ssafy.triplet.user.domain.User;
+import com.ssafy.triplet.user.dto.UserDto;
+import com.ssafy.triplet.user.repository.UserRepository;
 
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+	private final UserRepository userRepository;
+
+	public void signup(UserDto userDto) {
+		User user = User.toUserEntity(userDto);
+		userRepository.save(user);
+	}
 }
