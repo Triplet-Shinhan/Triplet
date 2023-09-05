9a3963280a493df075cd236e7b7d926c07507cbc
takealook <takealook97@naver.com>
2023-09-06 03:08:16 +0900

    feat: UserController에 login 기능 추가
    body: optional에 null이 들어있다면 그대로 머무르고 아니라면 api/trips(프로젝트 조회 페이지)로 이동한다.

--- Backend/triplet/src/main/java/com/ssafy/triplet/user/controller/UserController.java
+++ Backend/triplet/src/main/java/com/ssafy/triplet/user/controller/UserController.java
@@ -4,12 +4,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.ssafy.triplet.user.dto.UserDto;
 import com.ssafy.triplet.user.service.UserService;
 
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 
 // @RestController
@@ -25,8 +25,23 @@ public class UserController {
 	}
 
 	@PostMapping("/signup")
-	public ResponseEntity<String> saveUser(@RequestBody UserDto userDto) {
+	public ResponseEntity<String> saveUser(UserDto userDto) {
 		userService.signup(userDto);
 		return ResponseEntity.ok("User saved successfully");
+	}
+
+	@GetMapping("/login")
+	public String login() {
+		return "login";
+	}
+
+	@PostMapping("/login")
+	public String login(UserDto userDto, HttpSession session) {
+		return userService.login(userDto)
+			.map(loginResult -> {
+				session.setAttribute("email", loginResult.getEmail());
+				return "api/trips"; //프로젝트 조회 페이지로 이동
+			})
+			.orElse("login");// 그대로 로그인 페이지
 	}
 }
