487c369f16252d9c84858429756ed7321d3a0e0d
takealook <takealook97@naver.com>
2023-09-06 01:13:33 +0900

    fix: User 도메인 Column 어노테이션 삭제

--- Backend/triplet/src/main/java/com/ssafy/triplet/user/domain/User.java
+++ Backend/triplet/src/main/java/com/ssafy/triplet/user/domain/User.java
@@ -1,6 +1,5 @@
 package com.ssafy.triplet.user.domain;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -11,6 +10,7 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
 @Entity
 @Getter
@@ -19,21 +19,15 @@ import lombok.Setter;
 @Table(name = "users")
 @AllArgsConstructor
 @RequiredArgsConstructor
+@ToString
 public class User {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY) //MySQL의 AUTO_INCREMENT를 사용
 	private Long userId;
-
-	@Column(name = "name")
 	private String name;
-	@Column(name = "birth")
 	private String birth;
-	@Column(name = "email")
 	private String email;
-	@Column(name = "password")
 	private String password;
-	@Column(name = "phoneNum")
 	private String phoneNum;
-	@Column(name = "accountNum")
 	private String accountNum;
 }
