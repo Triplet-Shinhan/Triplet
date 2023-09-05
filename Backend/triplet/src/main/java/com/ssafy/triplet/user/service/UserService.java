5b947c3bb912cf24764f3c53f5f894a28e31741b
takealook <takealook97@naver.com>
2023-09-06 05:40:00 +0900

    fix: 로그인 DTO로 로그인 검증하도록 변경

--- Backend/triplet/src/main/java/com/ssafy/triplet/user/service/UserService.java
+++ Backend/triplet/src/main/java/com/ssafy/triplet/user/service/UserService.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 import org.springframework.stereotype.Service;
 
 import com.ssafy.triplet.user.domain.User;
+import com.ssafy.triplet.user.dto.LoginDto;
 import com.ssafy.triplet.user.dto.UserDto;
 import com.ssafy.triplet.user.repository.UserRepository;
 
@@ -20,9 +21,8 @@ public class UserService {
 		userRepository.save(user);
 	}
 
-	public Optional<UserDto> login(UserDto userDto) {
-		return userRepository.findByEmail(userDto.getEmail())
-			.filter(user -> user.getPassword().equals(userDto.getPassword()))
-			.map(UserDto::toUserDto);
+	public Optional<User> login(LoginDto loginDto) {
+		return userRepository.findByEmail(loginDto.getEmail())
+			.filter(user -> user.getPassword().equals(loginDto.getPassword()));
 	}
 }
