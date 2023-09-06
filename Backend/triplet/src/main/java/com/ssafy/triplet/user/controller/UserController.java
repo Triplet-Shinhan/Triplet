70de88df3da1170f0fb73e05698b930ba4c30bb2
takealook <takealook97@naver.com>
2023-09-06 21:36:34 +0900

    fix: 회원가입 파라미터 RequestBody 어노테이션 추가

--- Backend/triplet/src/main/java/com/ssafy/triplet/user/controller/UserController.java
+++ Backend/triplet/src/main/java/com/ssafy/triplet/user/controller/UserController.java
@@ -7,6 +7,7 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.ssafy.triplet.user.dto.LoginDto;
@@ -32,7 +33,7 @@ public class UserController {
 	}
 
 	@PostMapping("/signup")
-	public ResponseEntity<String> saveUser(UserDto userDto) {
+	public ResponseEntity<String> saveUser(@RequestBody UserDto userDto) {
 		userService.signup(userDto);
 		return ResponseEntity.ok("User saved successfully");
 	}
