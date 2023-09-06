f733dc75e7d5456fe9a37c3e6317c6fb575d1c5d
fangdol888 <fangdol888@gmail.com>
2023-09-06 20:31:01 +0900

    feat : 환전 관련 유틸 위한 클래스 선언 및 거리 차이 구하는 메소드 기본 틀 생성

---
+++ Backend/triplet/src/main/java/com/ssafy/triplet/exchange/util/ExchangeUtil.java
@@ -0,0 +1,14 @@
+package com.ssafy.triplet.exchange.util;
+
+import org.springframework.stereotype.Component;
+
+import com.ssafy.triplet.exchange.dto.NearBranchRequestDto;
+
+@Component
+public class ExchangeUtil {
+    public Double getDistance(NearBranchRequestDto dto, Double latitude, Double longitude) {
+        Double result = 0.0;
+
+        return result;
+    }
+}
