ec8b8e9f4cf1af49664c569b0bfee0a1d908be5d
fangdol888 <fangdol888@gmail.com>
2023-09-06 20:33:55 +0900

    feat : 좌표간 거리 구하는 메소드 구현

--- Backend/triplet/src/main/java/com/ssafy/triplet/exchange/util/ExchangeUtil.java
+++ Backend/triplet/src/main/java/com/ssafy/triplet/exchange/util/ExchangeUtil.java
@@ -7,8 +7,9 @@ import com.ssafy.triplet.exchange.dto.NearBranchRequestDto;
 @Component
 public class ExchangeUtil {
     public Double getDistance(NearBranchRequestDto dto, Double latitude, Double longitude) {
-        Double result = 0.0;
-
-        return result;
+        Double distance = 0.0;
+        distance = Math.sqrt((dto.getLatitude() - latitude) * (dto.getLatitude() - latitude)
+                + (dto.getLongitude() - longitude) * (dto.getLongitude() - longitude));
+        return distance;
     }
 }
