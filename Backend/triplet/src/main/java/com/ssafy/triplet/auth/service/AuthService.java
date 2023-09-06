ae2c1acc269a47cfb7ba896a91cc2f50caa40d85
takealook <takealook97@naver.com>
2023-09-07 00:54:06 +0900

    feat: 1원이체 시 받는 데이터로 파라미터 변경 및 랜덤 단어 추가

--- Backend/triplet/src/main/java/com/ssafy/triplet/auth/service/AuthService.java
+++ Backend/triplet/src/main/java/com/ssafy/triplet/auth/service/AuthService.java
@@ -1,18 +1,31 @@
 package com.ssafy.triplet.auth.service;
 
-import com.ssafy.triplet.auth.dto.AuthRequestDto;
-import com.ssafy.triplet.auth.dto.AuthResponseDto;
-import lombok.RequiredArgsConstructor;
+import java.util.Optional;
+
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
+import com.ssafy.triplet.auth.dto.AuthResponseDto;
+import com.ssafy.triplet.auth.dto.OneTransferRequestDto;
+import com.ssafy.triplet.auth.util.AuthUtility;
+
+import lombok.RequiredArgsConstructor;
+
+/*
+1. 프론트에서 (“이름”,”계좌번호”) 백엔드단으로 전송하고 백엔드에서 랜덤한 입금자명 알려줌
+2. 백에서 해당정보로 신한api(입금자명은 백에서지정)에 전송
+3. 신한api는 해당계좌로 1원이체
+4. 사용자는 프론트단에서 입금자명입력
+5. 프론트는 사용자입력이 맞는지 확인
+ */
 
 @Service
 @RequiredArgsConstructor
 public class AuthService {
+	private AuthUtility authUtility;
 
-    //1원이체 api
-    public Optional<AuthResponseDto> getOneTransferResponse(AuthRequestDto request) {
-        return Optional.of(null);
-    }
+	//1원이체 api
+	public Optional<AuthResponseDto> getOneTransferResponse(OneTransferRequestDto oneTransferDto) {
+		String randomWord = authUtility.getRandomWord();
+		return Optional.of(null);
+	}
 }
