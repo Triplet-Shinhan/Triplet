2a56bae78a4dd2316fbb014e7fda7c2d2edffb2a
takealook <takealook97@naver.com>
2023-09-08 01:39:00 +0900

    fix: RestController로 변경 및 리턴타입 json으로 변경

--- Backend/triplet/src/main/java/com/ssafy/triplet/auth/controller/AuthController.java
+++ Backend/triplet/src/main/java/com/ssafy/triplet/auth/controller/AuthController.java
@@ -1,26 +1,37 @@
 package com.ssafy.triplet.auth.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.ssafy.triplet.auth.dto.OneTransferRequestDto;
 import com.ssafy.triplet.auth.service.AuthService;
 import com.ssafy.triplet.auth.util.AuthUtility;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequiredArgsConstructor
-@Controller
+@RestController
 @RequestMapping("/v1/auth")
 public class AuthController {
-    private final AuthService authService;
-    private final AuthUtility authUtility;
-    @PostMapping("1transfer")
-    public String transferOne(@Validated @ModelAttribute OneTransferRequestDto oneTransferRequestDto) {
-        String memo = authUtility.getRandomWord();
-        authService.sendOne(oneTransferRequestDto, memo);
-        return memo;
-    }
+	private final AuthService authService;
+	private final AuthUtility authUtility;
+
+	@PostMapping("1transfer")
+	public ResponseEntity<Map<String, String>> transferOne(
+		@Validated @ModelAttribute OneTransferRequestDto oneTransferRequestDto) {
+		String memo = authUtility.getRandomWord();
+		authService.sendOne(oneTransferRequestDto, memo);
+
+		Map<String, String> responseMap = new HashMap<>();
+		responseMap.put("memo", memo);
+
+		return ResponseEntity.ok(responseMap);
+	}
 }
