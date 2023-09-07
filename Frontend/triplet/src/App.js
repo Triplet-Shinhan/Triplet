951c0eae205e9dba3d4f33dccc98108209e1be4e
Lainlnya <kimyelim100@gmail.com>
2023-09-07 23:42:34 +0900

    feat: add DiaryApiContext

--- Frontend/triplet/src/App.js
+++ Frontend/triplet/src/App.js
@@ -3,6 +3,9 @@ import './App.css';
 import Signup from './components/Opening/Signup/Signup';
 import Login from './components/Opening/Login/Login';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import DiaryInto from './components/Diary/DiaryInto/DiaryInto';
+import DiarySetUp from './components/Diary/DiarySetUp/DiarySetUp';
+import { DiaryApiProvider } from './context/DiaryApiContext';
 
 const queryClient = new QueryClient();
 
@@ -10,18 +13,22 @@ const router = createBrowserRouter([
   {
     path: '/',
     children: [
-      { index: true, element: <Login /> },
+      { index: true, element: <DiaryInto /> },
       { path: '/login', element: <Login /> },
       { path: '/signup', element: <Signup /> },
+      { path: '/trips', element: <DiaryInto /> },
+      { path: '/trips/setup', element: <DiarySetUp /> },
     ],
   },
 ]);
 
 function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router} />
-    </QueryClientProvider>
+    <DiaryApiProvider>
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
+    </DiaryApiProvider>
   );
 }
 
