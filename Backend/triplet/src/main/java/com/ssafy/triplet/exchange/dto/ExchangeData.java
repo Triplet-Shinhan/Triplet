f2fe2dbefe157361b01972d77318e054988d29a0
fangdol888 <fangdol888@gmail.com>
2023-09-07 21:42:15 +0900

    chore : 편의 위한 우대율 데이터 타입 String으로 변경

--- Backend/triplet/src/main/java/com/ssafy/triplet/exchange/dto/ExchangeData.java
+++ Backend/triplet/src/main/java/com/ssafy/triplet/exchange/dto/ExchangeData.java
@@ -7,5 +7,5 @@ public class ExchangeData {
     private String currencyCode; // 통화 코드
     private Double exchangeRate; // 환율
     private Long exchangeUnit; // 최소 단위
-    private Integer preferentialRate; // 우대율
+    private String preferentialRate; // 우대율
 }
