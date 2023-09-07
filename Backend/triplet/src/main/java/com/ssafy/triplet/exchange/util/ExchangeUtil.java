d526ea672f02dcf928d9e677e77cd8403ebbb8ac
fangdol888 <fangdol888@gmail.com>
2023-09-07 21:41:50 +0900

    fix : GPS 기반 두 지점 거리 계산 메소드 로직 수정

--- Backend/triplet/src/main/java/com/ssafy/triplet/exchange/util/ExchangeUtil.java
+++ Backend/triplet/src/main/java/com/ssafy/triplet/exchange/util/ExchangeUtil.java
@@ -2,14 +2,28 @@ package com.ssafy.triplet.exchange.util;
 
 import org.springframework.stereotype.Component;
 
-import com.ssafy.triplet.exchange.dto.NearBranchRequestDto;
-
 @Component
 public class ExchangeUtil {
-    public Double getDistance(NearBranchRequestDto dto, Double latitude, Double longitude) {
-        Double distance = 0.0;
-        distance = Math.sqrt((dto.getLatitude() - latitude) * (dto.getLatitude() - latitude)
-                + (dto.getLongitude() - longitude) * (dto.getLongitude() - longitude));
+
+    // 두 위도, 경도를 통한 계산
+    public Double getDistance(Double userLat, Double userLong, Double branchLat, Double branchLong) {
+        Double theta = userLong - branchLong;
+        Double distance = Math.sin(deg2rad(userLat)) * Math.sin(deg2rad(branchLat))
+                + Math.cos(deg2rad(userLat)) * Math.cos(deg2rad(branchLat)) * Math.cos(deg2rad(theta));
+
+        distance = Math.acos(distance);
+        distance = rad2deg(distance);
+        distance = distance * 60 * 1.1515;
+        // 미터법
+        distance *= 1609.344;
         return distance;
+    }
+
+    public Double deg2rad(Double deg) {
+        return (deg * Math.PI / 180.0);
+    }
+
+    public Double rad2deg(Double rad) {
+        return (rad * 180 / Math.PI);
     }
 }
