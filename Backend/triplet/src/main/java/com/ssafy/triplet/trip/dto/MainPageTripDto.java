7edbadd48db95bac8696982c077da62b85bd110e
takealook <takealook97@naver.com>
2023-09-10 01:41:13 +0900

    feat: 메인페이지용 trip dto 클래스 생성

---
+++ Backend/triplet/src/main/java/com/ssafy/triplet/trip/dto/MainPageTripDto.java
@@ -0,0 +1,28 @@
+package com.ssafy.triplet.trip.dto;
+
+import java.util.Date;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class MainPageTripDto {
+	@JsonProperty("location")
+	private String location;
+
+	@JsonProperty("prjName")
+	private String prjName;
+
+	@JsonProperty("startDate")
+	private Date startDate;
+
+	@JsonProperty("endDate")
+	private Date endDate;
+}
