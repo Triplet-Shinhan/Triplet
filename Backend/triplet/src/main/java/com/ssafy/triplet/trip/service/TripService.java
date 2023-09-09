81eabc6778e687bb30dc3ec72446a4a9812f6b2c
takealook <takealook97@naver.com>
2023-09-10 03:30:07 +0900

    feat: 여행 프로젝트 수정 메서드 구현 및 유효성 검증 추가

--- Backend/triplet/src/main/java/com/ssafy/triplet/trip/service/TripService.java
+++ Backend/triplet/src/main/java/com/ssafy/triplet/trip/service/TripService.java
@@ -5,10 +5,14 @@ import java.util.List;
 
 import org.springframework.stereotype.Service;
 
+import com.ssafy.triplet.exception.BaseException;
+import com.ssafy.triplet.exception.ErrorCode;
 import com.ssafy.triplet.trip.domain.Trip;
 import com.ssafy.triplet.trip.dto.MainPageTripDto;
 import com.ssafy.triplet.trip.dto.TripDto;
+import com.ssafy.triplet.trip.dto.TripEditDto;
 import com.ssafy.triplet.trip.repository.TripRepository;
+import com.ssafy.triplet.trip.util.TripValidation;
 import com.ssafy.triplet.user.repository.UserRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -18,6 +22,7 @@ import lombok.RequiredArgsConstructor;
 public class TripService {
 	private final UserRepository userRepository;
 	private final TripRepository tripRepository;
+	private final TripValidation tripValidation;
 
 	//메인화면 프로젝트 조회
 	public List<MainPageTripDto> getAllTrips(Long userId) {
@@ -36,6 +41,9 @@ public class TripService {
 
 	//프로젝트 생성
 	public void saveTrip(TripDto tripDto) {
+		//유효성 검증
+		tripValidation.checkCreateValid(tripDto);
+		//생성
 		Trip trip = toTripEntity(tripDto);
 		tripRepository.save(trip);
 	}
@@ -47,6 +55,21 @@ public class TripService {
 		tripRepository.deleteById(tripId);
 	}
 
+	public void editTrip(Long tripId, TripEditDto tripEditDto) {// 여행 시작날짜, 여행 종료날짜, 환율 변경
+		//유효성 검증
+		tripValidation.checkEditValid(tripEditDto);
+		// DB에서 해당 ID의 Trip 찾기
+		Trip trip = tripRepository.findById(tripId).orElse(null);
+		if(trip == null) {
+			throw new BaseException(ErrorCode.TRIP_ID_NOT_FOUND);
+		}
+		//수정
+		trip.setStartDate(tripEditDto.getStartDate());
+		trip.setEndDate(tripEditDto.getStartDate());
+		trip.setFixedRate(tripEditDto.getFixedRate());
+		tripRepository.save(trip);
+	}
+
 	private Trip toTripEntity(TripDto tripDto) {
 		Trip trip = new Trip();
 		trip.setUser(userRepository.findById(tripDto.getUserId()).orElse(null));
@@ -56,7 +79,7 @@ public class TripService {
 		trip.setExchangedBudget(tripDto.getExchangedBudget());
 		trip.setUsedBudget(tripDto.getUsedBudget());
 		trip.setCurrency(tripDto.getCurrency());
-		trip.setFixedBudget(tripDto.getFixedRate());
+		trip.setFixedRate(tripDto.getFixedRate());
 		trip.setStartDate(tripDto.getStartDate());
 		trip.setEndDate(tripDto.getEndDate());
 		return trip;
