02e0a7f4b629eb328f6065474a0b480b0de93ffa
takealook <takealook97@naver.com>
2023-09-09 23:13:27 +0900

    feat: TripService 클래스 생성 및 toTripEntity 메서드 구현

---
+++ Backend/triplet/src/main/java/com/ssafy/triplet/trip/service/TripService.java
@@ -0,0 +1,32 @@
+package com.ssafy.triplet.trip.service;
+
+import org.springframework.stereotype.Service;
+
+import com.ssafy.triplet.trip.domain.Trip;
+import com.ssafy.triplet.trip.dto.TripDto;
+import com.ssafy.triplet.trip.repository.TripRepository;
+import com.ssafy.triplet.user.repository.UserRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class TripService {
+	private final UserRepository userRepository;
+	private final TripRepository tripRepository;
+
+	private Trip toTripEntity(TripDto tripDto) {
+		Trip trip = new Trip();
+		trip.setUser(userRepository.findById(tripDto.getUserId()).orElse(null));
+		trip.setPrjName(tripDto.getPrjName());
+		trip.setLocation(tripDto.getLocation());
+		trip.setBudget(tripDto.getBudget());
+		trip.setExchangedBudget(tripDto.getExchangedBudget());
+		trip.setUsedBudget(tripDto.getUsedBudget());
+		trip.setCurrency(tripDto.getCurrency());
+		trip.setFixedBudget(tripDto.getFixedBudget());
+		trip.setStartDate(tripDto.getStartDate());
+		trip.setEndDate(tripDto.getEndDate());
+		return trip;
+	}
+}
