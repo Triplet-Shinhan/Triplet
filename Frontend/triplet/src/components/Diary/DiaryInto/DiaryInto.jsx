f6bed85a8dc36e6f5286df5c75eee3d763b221c1
Lainlnya <kimyelim100@gmail.com>
2023-09-09 20:03:12 +0900

    style: make tripDiary setup page and design

--- Frontend/triplet/src/components/Diary/DiaryInto/DiaryInto.jsx
+++ Frontend/triplet/src/components/Diary/DiaryInto/DiaryInto.jsx
@@ -3,6 +3,7 @@ import { useNavigate } from 'react-router-dom';
 import { useDiaryApi } from '../../../context/DiaryApiContext';
 import { useQuery } from '@tanstack/react-query';
 import DiaryPreview from '../DiaryPreview/DiaryPreview';
+import { IoIosArrowForward } from 'react-icons/io';
 import './DiaryInto.scss';
 
 export default function DiarySettings() {
@@ -11,50 +12,56 @@ export default function DiarySettings() {
   // 있으면 리스트 불러오기
   const navigate = useNavigate();
   // 다이어리 API 사용하겠다는 의미
-  const { diaryFunc } = useDiaryApi();
-
+  // const { diaryFunc } = useDiaryApi();
+  const tripList = undefined;
   // 백에서 diary api로 data 가져옴
-  const {
-    isLoading,
-    error,
-    data: tripList,
-  } = useQuery(
-    ['diary'],
-    () => {
-      return diaryFunc.viewProject();
-    },
-    { staleTime: 1000 * 6 * 1 }
-  );
+  // const {
+  //   isLoading,
+  //   error,
+  //   data: tripList,
+  // } = useQuery(
+  //   ['diary'],
+  //   () => {
+  //     return diaryFunc.viewProject();
+  //   },
+  //   { staleTime: 1000 * 6 * 1 }
+  // );
 
   return (
     <>
       <h1>안녕하세요</h1>
       <h2>신한 해커톤님</h2>
-      <ul>
-        <li>
-          <span>프로젝트 영역</span>
-          {/* tripList 형식 오는거 확인해볼 것 undefined인지 아닌지 */}
-          {tripList === undefined ? (
-            <span>
-              현재 생성된 프로젝트가 없습니다. 새 프로젝트를 생성해주세요.
-            </span>
-          ) : (
-            tripList.map((diary) => <DiaryPreview diaryInfo={diary} />)
-          )}
+      <ul className="tripInto">
+        <li className="tripInfo">
+          <nav>프로젝트</nav>
+          <div className="tripList">
+            {/* tripList 형식 오는거 확인해볼 것 undefined인지 아닌지 */}
+            {tripList === undefined ? (
+              <button onClick={() => navigate('/trips/setup')}>
+                현재 생성된 프로젝트가 없습니다. <br />새 프로젝트를
+                생성해주세요.
+              </button>
+            ) : (
+              tripList.map((diary) => <DiaryPreview diaryInfo={diary} />)
+            )}
+          </div>
         </li>
         <li>
-          <button className="goBtn goExchange">
-            환전신청하러가기
-            <span className="next">&gt;</span>
-          </button>
-        </li>
-        <li>
-          <button
-            className="goBtn makeNew"
-            onClick={() => navigate('/trips/setup')}
-          >
-            새 프로젝트 생성하기
-          </button>
+          <li>
+            <button className="goBtn goExchange">
+              <span>환전신청 하러 가기</span>
+              <IoIosArrowForward className="next" />
+            </button>
+          </li>
+          <li>
+            <button
+              className="goBtn makeNew"
+              onClick={() => navigate('/trips/setup')}
+            >
+              <span>새 프로젝트 생성하기</span>
+              <IoIosArrowForward className="next yellow" />
+            </button>
+          </li>
         </li>
       </ul>
     </>
--- Frontend/triplet/src/components/Diary/DiarySetUp/DiarySetUp.jsx
+++ Frontend/triplet/src/components/Diary/DiarySetUp/DiarySetUp.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './DiarySetUp.scss';
 
 export default function DiarySetUp() {
   const budget = [
@@ -22,23 +23,121 @@ export default function DiarySetUp() {
     '인도루피(INR)',
     '아랍에밀리트 디르함(AED)',
   ];
-  return (
-    <form>
-      <label for="projectName">프로젝트 이름을 설정해주세요</label>
-      <input type="text" id="projectName" />
 
-      <label for="tripPlace">여행지를 적어주세요</label>
-      <input type="text" id="tripPlace" />
-      {/* 정해져있나 통화가? */}
-      <label for="currency">통화설정을 해주세요</label>
-      <input type="" id="currency" />
-      <label for="budget">예산을 알려주세요</label>
-      <select name="budget" id="budget">
-        {budget.map((v) => (
-          <option value={v}>{v}</option>
-        ))}
-      </select>
-      <label>여행 기간을 알려주세요</label>
-    </form>
+  const [tripInfo, setTripInfo] = useState({
+    tripName: '',
+    tripPlace: '',
+    tripCurrency: '',
+    tripBudget: '',
+    startDate: '',
+    endDate: '',
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setTripInfo((trip) => ({ ...trip, [name]: value }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  // 프로젝트 정보 넘기기
+
+  return (
+    <>
+      <main className="diarySetupMain">
+        <h1>안녕하세요</h1>
+        <h2>신한 해커톤님</h2>
+        <form className="diaryForm" action="POST" onSubmit={handleSubmit}>
+          <figure className="logoArea">
+            <img
+              className="logo"
+              src="../../../assets/icons/shinhan-symbol.png"
+              alt="logo"
+            />
+            <figcaption>Triplet</figcaption>
+          </figure>
+          <div>
+            <label htmlFor="tripName">프로젝트 이름을 설정해주세요</label>
+            <input
+              type="text"
+              id="tripName"
+              name="tripName"
+              value={tripInfo.tripName}
+              onChange={handleChange}
+              placeholder="프로젝트 이름"
+              required
+            />
+          </div>
+          <div>
+            <label htmlFor="tripCurrency">통화설정을 해주세요</label>
+            <select
+              name="tripCurrency"
+              id="tripCurrency"
+              value={tripInfo.tripCurrency}
+              onChange={handleChange}
+              required
+            >
+              {budget.map((v) => (
+                <option value={v}>{v}</option>
+              ))}
+            </select>
+          </div>
+          <div>
+            <label htmlFor="tripDate" className="tripDate">
+              여행 기간을 알려주세요
+            </label>
+            <input
+              type="date"
+              id="tripDate"
+              name="startDate"
+              value={tripInfo.startDate}
+              onChange={handleChange}
+              required
+            />
+            <input
+              type="date"
+              id="tripDate"
+              name="endDate"
+              value={tripInfo.endDate}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div>
+            <label htmlFor="tripPlace">여행지를 적어주세요</label>
+            <input
+              type="text"
+              id="tripPlace"
+              name="tripPlace"
+              placeholder="여행지 이름"
+              value={tripInfo.tripPlace}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div>
+            <label htmlFor="tripBudget">예산을 알려주세요</label>
+            <input
+              type="text"
+              id="tripBudget"
+              name="tripBudget"
+              placeholder="₩250,000"
+              value={tripInfo.tripBudget}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <button>
+            <div className="makeDiary">생성하기</div>
+          </button>
+          <div className="boardingArea">boarding pass</div>
+        </form>
+        <div className="backgroundForm">
+          <div className="backgroundFooter">Boarding Pass</div>
+        </div>
+      </main>
+    </>
   );
 }
