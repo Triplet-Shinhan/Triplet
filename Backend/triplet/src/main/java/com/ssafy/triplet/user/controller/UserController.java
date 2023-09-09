d043ca567648be66014c52cbb57c8aac4beffe07
takealook <takealook97@naver.com>
2023-09-09 18:55:55 +0900

    remove: Valid 어노테이션 관련 기능 삭제

--- Backend/triplet/src/main/java/com/ssafy/triplet/user/controller/UserController.java
+++ Backend/triplet/src/main/java/com/ssafy/triplet/user/controller/UserController.java
@@ -18,7 +18,6 @@ import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -28,13 +27,13 @@ public class UserController {
 	private final UserService userService;
 
 	@PostMapping("/signup")
-	public ResponseEntity<ApiResponse> saveUser(@Valid @RequestBody UserDto userDto) {
+	public ResponseEntity<ApiResponse> saveUser(@RequestBody UserDto userDto) {
 		userService.signup(userDto);
 		return ResponseEntity.ok().build();//헤더에만 성공 코드
 	}
 
 	@PostMapping("/login")
-	public ResponseEntity<ApiResponse> login(@Valid @RequestBody LoginDto loginDto, HttpServletRequest request,
+	public ResponseEntity<ApiResponse> login(@RequestBody LoginDto loginDto, HttpServletRequest request,
 		HttpServletResponse response) {
 		User loginUser = userService.login(loginDto).orElse(null);
 		if (loginUser == null) {
@@ -55,7 +54,7 @@ public class UserController {
 	}
 
 	@PostMapping("/logout")
-	public ResponseEntity<ApiResponse> logout(@Valid HttpServletRequest request, HttpServletResponse response) {
+	public ResponseEntity<ApiResponse> logout(HttpServletRequest request, HttpServletResponse response) {
 		HttpSession session = request.getSession(false);
 		if (session != null) {
 			session.invalidate();
--- Backend/triplet/src/main/java/com/ssafy/triplet/user/domain/User.java
+++ Backend/triplet/src/main/java/com/ssafy/triplet/user/domain/User.java
@@ -8,9 +8,6 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -30,33 +27,21 @@ public class User {
 	private Long userId;
 
 	@Column(nullable = false)
-	@NotBlank(message = "공백이 포함되어 있습니다.")
-	@Pattern(regexp = "^[가-힣]{2,4}$", message = "이름은 2~4글자의 한글로 입력해주세요.")
 	private String name;
 
 	@Column(nullable = false)
-	@NotBlank(message = "공백이 포함되어 있습니다.")
-	@Pattern(regexp = "^(19|20)\\d\\d(0[1-9]|1[0-2])(0[1-9]|[12][0-9]|3[01])$", message = "올바른 생년월일 형식(8자리 년/월/일)을 입력하세요.")
 	private String birth;
 
 	@Column(nullable = false)
-	@NotBlank(message = "공백이 포함되어 있습니다.")
-	@Email(message = "이메일 형식에 맞게 입력해주세요.")
 	private String email;
 
 	@Column(nullable = false)
-	@NotBlank(message = "공백이 포함되어 있습니다.")
-	@Pattern(regexp = "^(?=.*[A-Za-z])(?=.*[!@#$%^*+=-]).{8,15}$", message = "비밀번호는 8~15자리이며, 특수문자를 하나 이상을 포함해야 합니다.")
 	private String password;
 
 	@Column(nullable = false)
-	@NotBlank(message = "공백이 포함되어 있습니다.")
-	@Pattern(regexp = "^[0-9]{3}[0-9]{4}[0-9]{4}$", message = "올바르지 않은 전화번호 형식입니다. 형식에 맞게 핸드폰 번호 11자리를 입력해주세요.")
 	private String phoneNum;
 
 	@Column(nullable = false)
-	@NotBlank(message = "공백이 포함되어 있습니다.")
-	@Pattern(regexp = "^[0-9]+$", message = "계좌번호에는 숫자만 입력할 수 있습니다.")
 	private String accountNum;
 
 	public User toUserEntity(UserDto userDto) {
