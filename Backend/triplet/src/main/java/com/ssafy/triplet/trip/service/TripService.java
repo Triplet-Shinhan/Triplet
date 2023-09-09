8725acc67879d08c431bb126ca31132700894e65
takealook <takealook97@naver.com>
2023-09-10 01:43:27 +0900

    fix: TripService의 toEntity 메서드 삭제 및 메인화면으로 전달할 getAllTrips 메서드 구현

--- Backend/triplet/src/main/java/com/ssafy/triplet/trip/service/TripService.java
+++ Backend/triplet/src/main/java/com/ssafy/triplet/trip/service/TripService.java
@@ -1,39 +1,32 @@
 package com.ssafy.triplet.trip.service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
 
 import com.ssafy.triplet.trip.domain.Trip;
-import com.ssafy.triplet.trip.dto.LoginUserDto;
-import com.ssafy.triplet.trip.dto.TripDto;
+import com.ssafy.triplet.trip.dto.MainPageTripDto;
 import com.ssafy.triplet.trip.repository.TripRepository;
-import com.ssafy.triplet.user.repository.UserRepository;
 
 import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
 public class TripService {
-	private final UserRepository userRepository;
 	private final TripRepository tripRepository;
 
-	private Trip toTripEntity(TripDto tripDto) {
-		Trip trip = new Trip();
-		trip.setUser(userRepository.findById(tripDto.getUserId()).orElse(null));
-		trip.setPrjName(tripDto.getPrjName());
-		trip.setLocation(tripDto.getLocation());
-		trip.setBudget(tripDto.getBudget());
-		trip.setExchangedBudget(tripDto.getExchangedBudget());
-		trip.setUsedBudget(tripDto.getUsedBudget());
-		trip.setCurrency(tripDto.getCurrency());
-		trip.setFixedBudget(tripDto.getFixedBudget());
-		trip.setStartDate(tripDto.getStartDate());
-		trip.setEndDate(tripDto.getEndDate());
-		return trip;
-	}
-
-	public List<Trip> findAllTrips(LoginUserDto loginUserDto) {
-		return tripRepository.findAllByUser_Id(loginUserDto.getUserId());
+	public List<MainPageTripDto> getAllTrips(Long userId) {
+		List<Trip> trips = tripRepository.findAllByUserId(userId);
+		List<MainPageTripDto> mainPageTrips = new ArrayList<>();
+		for (Trip trip : trips) {
+			MainPageTripDto mainPageTripDto = new MainPageTripDto();
+			mainPageTripDto.setPrjName(trip.getPrjName());
+			mainPageTripDto.setLocation(trip.getLocation());
+			mainPageTripDto.setStartDate(trip.getStartDate());
+			mainPageTripDto.setEndDate(trip.getEndDate());
+			mainPageTrips.add(mainPageTripDto);
+		}
+		return mainPageTrips;
 	}
 }
