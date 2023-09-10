53b6b2ee1493841f5b89baa9c01b67a41be31dd6
takealook <takealook97@naver.com>
2023-09-10 16:29:55 +0900

    refactor: jsonproperty 명시 및 변수명 테이블대로 수정

--- Backend/triplet/src/main/java/com/ssafy/triplet/auth/dto/OneTransferRequestDto.java
+++ Backend/triplet/src/main/java/com/ssafy/triplet/auth/dto/OneTransferRequestDto.java
@@ -1,5 +1,7 @@
 package com.ssafy.triplet.auth.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,6 +12,9 @@ import lombok.Setter;
 @NoArgsConstructor
 @AllArgsConstructor
 public class OneTransferRequestDto {
+	@JsonProperty("name")
 	private String name;
-	private String account;
+
+	@JsonProperty("accountNum")
+	private String accountNum;
 }
