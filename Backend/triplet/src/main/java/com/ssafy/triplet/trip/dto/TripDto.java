2c4b70434c60ea1d3ceb761de4f2eacb08e21dec
takealook97 <takealook97@naver.com>
2023-09-12 22:47:00 +0900

    refactor: 프로젝트 생성 시 불필요한 사용예산 및 환율 필드 삭제

--- Backend/triplet/src/main/java/com/ssafy/triplet/trip/dto/TripDto.java
+++ Backend/triplet/src/main/java/com/ssafy/triplet/trip/dto/TripDto.java
@@ -27,14 +27,8 @@ public class TripDto {
 	@JsonProperty("exchangedBudget")
 	private Long exchangedBudget;
 
-	@JsonProperty("usedBudget")//보류
-	private Long usedBudget;
-
 	@JsonProperty("currency")
 	private String currency;
-
-	@JsonProperty("fixedRate")//보류
-	private Float fixedRate;
 
 	@JsonProperty("startDate")
 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
