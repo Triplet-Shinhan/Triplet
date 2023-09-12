96d9dc91e0d3ba535d0cbc16ade98c5de27f8b94
Lainlnya <kimyelim100@gmail.com>
2023-09-12 22:31:55 +0900

    chore: modify netlify.toml

--- Frontend/triplet/src/api/AccountApis.js
+++ Frontend/triplet/src/api/AccountApis.js
@@ -1,18 +1,17 @@
 import axios from 'axios';
 
+const PROXY = window.location.hostname === 'localhost' ? '' : '/proxy';
+
 export const AxiosInstance = axios.create({
   withCredentials: true,
   baseURL: process.env.REACT_APP_TRIPLET_SERVER_IP,
 });
 
 export const checkAccount = ({ name, accountNum }) => {
-  return axios.post(
-    process.env.REACT_APP_TRIPLET_SERVER_IP + '/v1/auth/1transfer',
-    {
-      name,
-      accountNum,
-    }
-  );
+  return axios.post(`${PROXY}/v1/auth/1transfer`, {
+    name,
+    accountNum,
+  });
 };
 
 export const signupUser = ({ formData }) => {
