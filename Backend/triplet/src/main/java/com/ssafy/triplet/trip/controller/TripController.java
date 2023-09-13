5428c018002e9378ddddad4bf1ec32eb809dcb25
takealook <takealook97@naver.com>
2023-09-13 17:27:02 +0900

    feat: TripResponse로 리턴타입 통일 및 프로젝트 생성 및 수정 시 tripId 리턴하도록 변경

--- Backend/triplet/src/main/java/com/ssafy/triplet/trip/controller/TripController.java
+++ Backend/triplet/src/main/java/com/ssafy/triplet/trip/controller/TripController.java
@@ -1,97 +1,89 @@
 package com.ssafy.triplet.trip.controller;
 
-import java.util.List;
-
-import com.ssafy.triplet.user.repository.UserRepository;
-import com.ssafy.triplet.user.util.UserUtility;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.ssafy.triplet.exception.BaseException;
 import com.ssafy.triplet.exception.ErrorCode;
 import com.ssafy.triplet.trip.domain.Trip;
 import com.ssafy.triplet.trip.dto.MainPageTripDto;
 import com.ssafy.triplet.trip.dto.TripDto;
 import com.ssafy.triplet.trip.dto.TripEditDto;
+import com.ssafy.triplet.trip.response.TripResponse;
 import com.ssafy.triplet.trip.service.TripService;
 import com.ssafy.triplet.user.domain.User;
-
+import com.ssafy.triplet.user.repository.UserRepository;
+import com.ssafy.triplet.user.util.UserUtility;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/trips")
 public class TripController {
-	private final UserRepository userRepository;
-	private final TripService tripService;
-	private final UserUtility userUtility;
-	private final Logger logger = LoggerFactory.getLogger(TripController.class);
+    private final UserRepository userRepository;
+    private final TripService tripService;
+    private final UserUtility userUtility;
+    private final Logger logger = LoggerFactory.getLogger(TripController.class);
 
-	@GetMapping
-	public ResponseEntity<List<MainPageTripDto>> readTrips(HttpServletRequest request) {
-		HttpSession session = request.getSession();
-		logger.debug("readTrips request success");
-		if (session != null) {
-			User user = (User)session.getAttribute("user");
-			if (user != null) {
-				Long userId = user.getUserId();
-				logger.debug("readTrips success");
-				//회원 정보를 찾을 수 없을 경우 에러
-				userRepository.findById(userId).orElseThrow(() -> new BaseException(ErrorCode.USER_ID_NOT_FOUND));
-				return ResponseEntity.ok(tripService.getAllTrips(userId));
-			}
-		}
-		throw new BaseException(ErrorCode.USER_ID_NOT_FOUND);
-	}
+    @GetMapping
+    public ResponseEntity<List<MainPageTripDto>> readTrips(HttpServletRequest request) {
+        HttpSession session = request.getSession();
+        logger.debug("readTrips request success");
+        if (session != null) {
+            User user = (User) session.getAttribute("user");
+            if (user != null) {
+                Long userId = user.getUserId();
+                logger.debug("readTrips success");
+                //회원 정보를 찾을 수 없을 경우 에러
+                userRepository.findById(userId).orElseThrow(() -> new BaseException(ErrorCode.USER_ID_NOT_FOUND));
+                return ResponseEntity.ok(tripService.getAllTrips(userId));
+            }
+        }
+        throw new BaseException(ErrorCode.USER_ID_NOT_FOUND);
+    }
 
-	@PostMapping
-	public ResponseEntity<Trip> createTrip(@RequestBody TripDto tripDto, HttpServletRequest httpServletRequest) {
-		logger.debug("createTrip request success");
-		User loginUser = userUtility.getUserFromCookie(httpServletRequest);
-		tripService.saveTrip(tripDto, loginUser);
-		logger.debug("createTrip success");
-		return ResponseEntity.ok().build();
-	}
+    @PostMapping
+    public ResponseEntity<TripResponse> createTrip(@RequestBody TripDto tripDto, HttpServletRequest httpServletRequest) {
+        logger.debug("createTrip request success");
+        User loginUser = userUtility.getUserFromCookie(httpServletRequest);
+        Trip savedTrip = tripService.saveTrip(tripDto, loginUser);
+        logger.debug("createTrip success");
+        return ResponseEntity.ok(new TripResponse(savedTrip.getTripId()));
+    }
 
-	@DeleteMapping("/{tripId}")
-	public ResponseEntity<Trip> deleteTrip(@PathVariable Long tripId) {
-		logger.debug("deleteTrip request success");
-		tripService.removeTrip(tripId);
-		logger.debug("deleteTrip success");
-		return ResponseEntity.ok().build();
-	}
+    @DeleteMapping("/{tripId}")
+    public ResponseEntity<TripResponse> deleteTrip(@PathVariable Long tripId) {
+        logger.debug("deleteTrip request success");
+        tripService.removeTrip(tripId);
+        logger.debug("deleteTrip success");
+        return ResponseEntity.ok().build();
+    }
 
-	@PatchMapping("/{tripId}")
-	public ResponseEntity<Trip> updateTrip(@PathVariable Long tripId, @RequestBody TripEditDto tripEditDto) {
-		logger.debug("editTrip request success");
-		tripService.editTrip(tripId, tripEditDto);
-		logger.debug("editTrip success");
-		return ResponseEntity.ok().build();
-	}
+    @PatchMapping("/{tripId}")
+    public ResponseEntity<TripResponse> updateTrip(@PathVariable Long tripId, @RequestBody TripEditDto tripEditDto) {
+        logger.debug("editTrip request success");
+        tripService.editTrip(tripId, tripEditDto);
+        logger.debug("editTrip success");
+        return ResponseEntity.ok(new TripResponse(tripId));
+    }
 
-	// @GetMapping("/{tripId}/dailies")
-	// public ResponseEntity<List<Daily>> readDaily(@PathVariable Long tripId) {
-	// 	return null;
-	// }
-	//
-	// @GetMapping("{tripId}/dailies/{dailyId}")
-	// public ResponseEntity<List<Payment>> readPayment(@PathVariable Long tripId, @PathVariable Long dailyId) {
-	// 	return null;
-	// }
-	//
-	// @PostMapping("{tripId}/dailies/{dailyId}/images")
-	// public ResponseEntity<Trip> uploadImage(@PathVariable Long tripId, @PathVariable Long dailyId) {
-	// 	return null;
-	// }
+    // @GetMapping("/{tripId}/dailies")
+    // public ResponseEntity<List<Daily>> readDaily(@PathVariable Long tripId) {
+    // 	return null;
+    // }
+    //
+    // @GetMapping("{tripId}/dailies/{dailyId}")
+    // public ResponseEntity<List<Payment>> readPayment(@PathVariable Long tripId, @PathVariable Long dailyId) {
+    // 	return null;
+    // }
+    //
+    // @PostMapping("{tripId}/dailies/{dailyId}/images")
+    // public ResponseEntity<Trip> uploadImage(@PathVariable Long tripId, @PathVariable Long dailyId) {
+    // 	return null;
+    // }
 }
