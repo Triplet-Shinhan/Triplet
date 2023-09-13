887f751d7b52c226befe0f31154d7506ac192234
takealook <takealook97@naver.com>
2023-09-13 17:23:43 +0900

    refactor: API response 클래스 변경

---
+++ Backend/triplet/src/main/java/com/ssafy/triplet/user/response/UserApiResponse.java
@@ -0,0 +1,10 @@
+package com.ssafy.triplet.user.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class UserApiResponse {
+	private Long userId;
+}
