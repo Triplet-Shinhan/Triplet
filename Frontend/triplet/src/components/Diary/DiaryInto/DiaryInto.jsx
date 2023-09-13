7fb7b5bebb28bffa0d15a4b50cae615e0ec70210
Lainlnya <kimyelim100@gmail.com>
2023-09-13 16:46:37 +0900

    refactor: update DiaryInfo

--- Frontend/triplet/src/components/Diary/DiaryInto/DiaryInto.jsx
+++ Frontend/triplet/src/components/Diary/DiaryInto/DiaryInto.jsx
@@ -7,15 +7,9 @@ import { useDiaryApi } from '../../../context/DiaryApiContext';
 import './DiaryInto.scss';
 
 export default function DiarySettings() {
-  // 로그인 사용자 세션에서 가져오기 => 안녕하세요 뒤에 뿌리기
-  // 프로젝트 있는지 쿼리로 확인하고 없으면 현재 생성된 프로젝트가 없습니다. 새 프로젝트를 생성해주세요.
-  // 있으면 리스트 불러오기
   const navigate = useNavigate();
   // 다이어리 API 사용하겠다는 의미
   const { diary } = useDiaryApi();
-  // const tripList = undefined;
-
-  // 백에서 diary api로 data 가져옴
   const {
     isLoading,
     error,
@@ -39,7 +33,6 @@ export default function DiarySettings() {
           <div
             className={tripList === undefined ? 'tripList empty' : 'tripList'}
           >
-            {/* tripList 형식 오는거 확인해볼 것 undefined인지 아닌지 */}
             {tripList === undefined ? (
               <button onClick={() => navigate('/trips/setup')}>
                 현재 생성된 프로젝트가 없습니다. <br />새 프로젝트를
