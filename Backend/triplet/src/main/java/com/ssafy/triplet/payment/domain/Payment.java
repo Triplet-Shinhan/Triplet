807279d43aa59356cac654152d91faa5a2106c97
takealook97 <takealook97@naver.com>
2023-09-14 02:46:01 +0900

    refactor: Payment 엔티티 어노테이션 추가

--- Backend/triplet/src/main/java/com/ssafy/triplet/payment/domain/Payment.java
+++ Backend/triplet/src/main/java/com/ssafy/triplet/payment/domain/Payment.java
@@ -1,47 +1,58 @@
 package com.ssafy.triplet.payment.domain;
 
+import java.time.LocalDateTime;
+
 import com.ssafy.triplet.daily.domain.Daily;
 import com.ssafy.triplet.trip.domain.Trip;
-import com.ssafy.triplet.user.domain.User;
-import jakarta.persistence.*;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.time.LocalDateTime;
 @Getter
 @Setter
 @Builder
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
 @Table(name = "payment")
 public class Payment {
 
-    @Id
-    @GeneratedValue(strategy =  GenerationType.IDENTITY)
-    @Column(name = "payment_Id")
-    private Long paymentId;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "payment_Id")
+	private Long paymentId;
 
+	@ManyToOne
+	@JoinColumn(name = "trip_id")
+	private Trip trip;
 
-    @ManyToOne
-    @JoinColumn(name="trip_id")
-    private Trip trip;
+	@ManyToOne
+	@JoinColumn(name = "daily_id")
+	private Daily daily;
 
-    @ManyToOne
-    @JoinColumn(name="daily_id")
-    private Daily daily;
+	@Column(name = "item")
+	private String item;
 
-    @Column(name = "item")
-    private String item;
+	@Column(name = "cost")
+	private Long cost;
 
-    @Column(name = "cost")
-    private Long cost;
+	@Column(name = "foreign_currency")
+	private String foreignCurrency;
 
-    @Column(name = "foreign_currency")
-    private String foreignCurrency;
+	@Column(name = "date")
+	private LocalDateTime date;
 
-    @Column(name = "date")
-    private LocalDateTime date;
-
-    @Column(name = "method")
-    private String method;
+	@Column(name = "method")
+	private String method;
 }
