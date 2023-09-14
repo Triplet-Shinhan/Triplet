6955a07f7fd11786e5f5116c23b6ddfc3839c365
takealook <takealook97@naver.com>
2023-09-14 14:21:44 +0900

    remove: DailyController 주석 삭제

--- Backend/triplet/src/main/java/com/ssafy/triplet/daily/controller/DailyController.java
+++ Backend/triplet/src/main/java/com/ssafy/triplet/daily/controller/DailyController.java
@@ -1,45 +1,37 @@
 package com.ssafy.triplet.daily.controller;
 
-import java.util.List;
-
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.ssafy.triplet.daily.dto.DailyDto;
 import com.ssafy.triplet.daily.response.DailiesResponse;
 import com.ssafy.triplet.daily.response.DailyResponse;
 import com.ssafy.triplet.daily.service.DailyService;
 import com.ssafy.triplet.payment.domain.Payment;
-
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/trips")
 public class DailyController {
-	private final DailyService dailyService;
+    private final DailyService dailyService;
 
-	@GetMapping("/{tripId}/dailies")
-	public ResponseEntity<DailiesResponse> readDailies(@PathVariable Long tripId) {
-		//기록 조회 때마다 총지출 합산 로직
-		List<DailyDto> dailies = dailyService.toDailyDtoList(tripId);
-		return ResponseEntity.ok(new DailiesResponse(tripId, dailies));
-		//리턴 시 남은예산 및 계산 로직 구현해서 DailiesResponse 에 담아서 리턴하기!
-	}
+    @GetMapping("/{tripId}/dailies")
+    public ResponseEntity<DailiesResponse> readDailies(@PathVariable Long tripId) {
+        List<DailyDto> dailies = dailyService.toDailyDtoList(tripId);
+        return ResponseEntity.ok(new DailiesResponse(tripId, dailyService.getDashboard(tripId), dailies));
+    }
 
-	@GetMapping("/{tripId}/dailies/{dailyId}")
-	public ResponseEntity<List<Payment>> readDaily(@PathVariable Long tripId, @PathVariable Long dailyId) {
-		List<Payment> payments = dailyService.getPayments(dailyId);
-		return ResponseEntity.ok(payments);
-	}
+    @GetMapping("/{tripId}/dailies/{dailyId}")
+    public ResponseEntity<List<Payment>> readDaily(@PathVariable Long tripId, @PathVariable Long dailyId) {
+        List<Payment> payments = dailyService.getPayments(dailyId);
+        return ResponseEntity.ok(payments);
+    }
 
-	@PostMapping("{tripId}/dailies/{dailyId}/images")
-	public ResponseEntity<DailyResponse> saveImage(@PathVariable Long tripId, @PathVariable Long dailyId) { //S3
+    @PostMapping("{tripId}/dailies/{dailyId}/images")
+    public ResponseEntity<DailyResponse> saveImage(@PathVariable Long tripId, @PathVariable Long dailyId) { //S3
 
-		return null;
-	}
+        return null;
+    }
 }
