a11d7321f7012977250abd704c5a1a2406ea9479
yeongjin <hanyj0104@naver.com>
2023-09-15 02:20:41 +0900

    feat: payment 신한 거래내역 api 갱신로직(구현중)

--- Backend/triplet/src/main/java/com/ssafy/triplet/payment/service/PaymentService.java
+++ Backend/triplet/src/main/java/com/ssafy/triplet/payment/service/PaymentService.java
@@ -11,11 +11,15 @@ import com.ssafy.triplet.payment.repository.PaymentRepository;
 import com.ssafy.triplet.trip.domain.Trip;
 import com.ssafy.triplet.trip.repository.TripRepository;
 import com.ssafy.triplet.user.domain.User;
-import com.ssafy.triplet.user.repository.UserRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Collections;
 import java.util.List;
 
 import static com.ssafy.triplet.exception.ErrorCode.*;
@@ -29,7 +33,7 @@ public class PaymentService {
     private final DailyRepository dailyRepository;
     private final WebClientUtil webClientUtil;
 
-    public void createPayment(PaymentReqDto reqDto, User user){
+    public void createPayment(PaymentReqDto reqDto, User user) {
         Trip trip = tripRepository.findById(reqDto.getTripId()).orElseThrow(() -> new BaseException(TRIP_ID_NOT_FOUND));
         Daily daily = dailyRepository.findById(reqDto.getDailyId()).orElseThrow(() -> new BaseException(PAYMENT_ID_NOT_FOUND));
 
@@ -73,7 +77,7 @@ public class PaymentService {
 
         Trip trip = tripRepository.findById(payment.getTrip().getTripId()).orElseThrow(() -> new BaseException(TRIP_ID_NOT_FOUND));
         //본인지출 확인
-        if(trip.getUser().getUserId()!=user.getUserId()){
+        if (trip.getUser().getUserId() != user.getUserId()) {
             throw new BaseException(NOT_AUTHORIZED);
         }
 
@@ -82,13 +86,45 @@ public class PaymentService {
 
 
     //신한api에서 데이터 조회
-    public void updatePaymentList(User user,Long tripId){
+    public void updatePaymentList(User user, Long tripId) {
         AccountDataBody accountDataBody = webClientUtil.getAccount(user.getAccountNum());
         //해당 tripId에 해당하는 dailyList가져옴
-        Trip trip = tripRepository.findById(tripId).orElseThrow(()->new BaseException(TRIP_ID_NOT_FOUND));
+        Trip trip = tripRepository.findById(tripId).orElseThrow(() -> new BaseException(TRIP_ID_NOT_FOUND));
+        //payments의 최신값 보다 더 최신인 databody(카드결제)
+        List<Payment> payments = trip.getPayments();
+        if (payments != null) {
+
+        }
+            LocalDateTime lastDateTime=payments.get(0).getDate();
+
+        //가장 위에있는 최신값의 날짜 및 시간 가져오기
+        for(Payment payment: payments){
+            if(lastDateTime.isBefore(payment.getDate())){
+                lastDateTime=payment.getDate();
+            }
+        }
+        //dailyid
+
+        for(int index=0;index<payments.size();index++) {
+            String lastDateStr = accountDataBody.getTransactions().get(index).getTransactionDate();
+            String lastTimeStr = accountDataBody.getTransactions().get(index).getTransactionTime();
+            LocalDateTime paymentTime = StringToDateTime(lastDateStr+lastTimeStr);
+
+            if(!lastDateTime.isBefore(paymentTime))break;//갱신된데이터 보다 오래되었다면
+        }
+
 
 
         //db의 최신거래일자보다 최신날짜들만 비교
 
+    }
+
+    public LocalDateTime StringToDateTime(String dateString){
+        String ldtStr = dateString;
+        DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("yyyyMMddHHmmss");
+
+        LocalDateTime lastDateTime = LocalDateTime.parse(ldtStr, dateFormat);
+
+        return lastDateTime;
     }
 }
