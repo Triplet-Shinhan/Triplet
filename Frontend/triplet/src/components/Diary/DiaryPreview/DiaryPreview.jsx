68f5ad827e61039654523219a2626fa407e8cf22
Lainlnya <kimyelim100@gmail.com>
2023-09-14 21:18:59 +0900

    refactor: refactor trip preview

--- Frontend/triplet/src/components/Diary/DiaryPreview/DiaryPreview.jsx
+++ Frontend/triplet/src/components/Diary/DiaryPreview/DiaryPreview.jsx
@@ -5,12 +5,11 @@ import { useNavigate } from 'react-router-dom';
 export default function DiaryPreview({ diaryInfo }) {
   const navigate = useNavigate();
 
-  console.log(diaryInfo);
   return (
     <ul
       className="diaryPreview"
       onClick={() =>
-        navigate(`/trips/${diaryInfo.diaryId}/dailies`, { state: diaryInfo })
+        navigate(`/trips/${diaryInfo.tripId}/dailies`, { state: diaryInfo })
       }
     >
       <section className="diaryInfo">
