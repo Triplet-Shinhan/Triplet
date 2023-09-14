1f7aeca8878a45999d8edc33ce8348a63280de1c
takealook97 <takealook97@naver.com>
2023-09-15 02:34:15 +0900

    feat: S3를 통해 DailyController 내에 이미지 업로드 및 삭제 메서드 구현

--- Backend/triplet/src/main/java/com/ssafy/triplet/daily/controller/DailyController.java
+++ Backend/triplet/src/main/java/com/ssafy/triplet/daily/controller/DailyController.java
@@ -1,19 +1,24 @@
 package com.ssafy.triplet.daily.controller;
 
+import java.io.IOException;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.ssafy.triplet.daily.dto.DailyDto;
 import com.ssafy.triplet.daily.dto.PaymentDto;
 import com.ssafy.triplet.daily.response.DailiesResponse;
 import com.ssafy.triplet.daily.response.DailyResponse;
 import com.ssafy.triplet.daily.service.DailyService;
+import com.ssafy.triplet.daily.service.S3Service;
 
 import lombok.RequiredArgsConstructor;
 
@@ -22,6 +27,7 @@ import lombok.RequiredArgsConstructor;
 @RequestMapping("/api/trips")
 public class DailyController {
 	private final DailyService dailyService;
+	private final S3Service s3Service;
 
 	@GetMapping("/{tripId}/dailies")
 	public ResponseEntity<DailiesResponse> readDailies(@PathVariable Long tripId) {
@@ -36,8 +42,16 @@ public class DailyController {
 	}
 
 	@PostMapping("{tripId}/dailies/{dailyId}/images")
-	public ResponseEntity<DailyResponse> saveImage(@PathVariable Long tripId, @PathVariable Long dailyId) { //S3
+	public ResponseEntity<DailyResponse> saveImage(@PathVariable Long tripId, @PathVariable Long dailyId,
+		@RequestPart(value = "imageFile", required = false) MultipartFile multipartFile) throws IOException {
+		String url = s3Service.uploadImage(multipartFile);
+		//저장로직 추가
+		return ResponseEntity.ok().build();
+	}
 
-		return null;
+	@DeleteMapping("{tripId}/dailies/{dailyId}/images")
+	public ResponseEntity<DailyResponse> deleteImage(@PathVariable Long tripId, @PathVariable Long dailyId) {
+		s3Service.deleteImage(s3Service.getImageUrl(dailyId));
+		return ResponseEntity.ok().build();
 	}
 }
