38649c8f5baae09a61b810036df6017d591d3857
takealook <takealook97@naver.com>
2023-09-14 14:20:34 +0900

    refactor: Daily 전제 조회 시 대시보드 3가지 값 포함해서 리턴하도록 Response dto 수정

--- Backend/triplet/src/main/java/com/ssafy/triplet/daily/response/DailiesResponse.java
+++ Backend/triplet/src/main/java/com/ssafy/triplet/daily/response/DailiesResponse.java
@@ -2,14 +2,22 @@ package com.ssafy.triplet.daily.response;
 
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.ssafy.triplet.daily.dto.DailyDto;
 
+import com.ssafy.triplet.daily.dto.DashboardDto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
 @AllArgsConstructor
 public class DailiesResponse {
+	@JsonProperty("tripId")
 	private Long tripId;
+
+	@JsonProperty("dashboard")
+	private DashboardDto dashboardDto;
+
+	@JsonProperty("dailies")
 	private List<DailyDto> dailies;
 }
