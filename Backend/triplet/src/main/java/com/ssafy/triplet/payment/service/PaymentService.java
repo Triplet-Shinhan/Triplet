e26f3f3287d821d2c8d5d37509e8ce1544b60cfd
yeongjin <hanyj0104@naver.com>
2023-09-15 00:15:27 +0900

    feat: 거래정보 조회 로직 생성

--- Backend/triplet/src/main/java/com/ssafy/triplet/payment/service/PaymentService.java
+++ Backend/triplet/src/main/java/com/ssafy/triplet/payment/service/PaymentService.java
@@ -3,6 +3,8 @@ package com.ssafy.triplet.payment.service;
 import com.ssafy.triplet.daily.domain.Daily;
 import com.ssafy.triplet.daily.repository.DailyRepository;
 import com.ssafy.triplet.exception.BaseException;
+import com.ssafy.triplet.parser.WebClientUtil;
+import com.ssafy.triplet.parser.dto.acount.AccountDataBody;
 import com.ssafy.triplet.payment.domain.Payment;
 import com.ssafy.triplet.payment.dto.PaymentReqDto;
 import com.ssafy.triplet.payment.repository.PaymentRepository;
@@ -14,6 +16,8 @@ import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 import static com.ssafy.triplet.exception.ErrorCode.*;
 
 @Transactional
@@ -23,6 +27,7 @@ public class PaymentService {
     private final TripRepository tripRepository;
     private final PaymentRepository paymentRepository;
     private final DailyRepository dailyRepository;
+    private final WebClientUtil webClientUtil;
 
     public void createPayment(PaymentReqDto reqDto, User user){
         Trip trip = tripRepository.findById(reqDto.getTripId()).orElseThrow(() -> new BaseException(TRIP_ID_NOT_FOUND));
@@ -73,5 +78,17 @@ public class PaymentService {
         }
 
         paymentRepository.deleteById(payment.getPaymentId());
+    }
+
+
+    //신한api에서 데이터 조회
+    public void updatePaymentList(User user,Long tripId){
+        AccountDataBody accountDataBody = webClientUtil.getAccount(user.getAccountNum());
+        //해당 tripId에 해당하는 dailyList가져옴
+        Trip trip = tripRepository.findById(tripId).orElseThrow(()->new BaseException(TRIP_ID_NOT_FOUND));
+
+
+        //db의 최신거래일자보다 최신날짜들만 비교
+
     }
 }
