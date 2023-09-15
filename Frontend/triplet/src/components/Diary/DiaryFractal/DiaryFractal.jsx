44583f3d343cb4977a864c841ab6cc0874f9cb2d
Lainlnya <kimyelim100@gmail.com>
2023-09-15 22:09:04 +0900

    refactor: change DiarySetUp logic

--- Frontend/triplet/src/components/Diary/DiaryFractal/DiaryFractal.jsx
+++ Frontend/triplet/src/components/Diary/DiaryFractal/DiaryFractal.jsx
@@ -7,6 +7,7 @@ import { useDiaryApi } from '../../../context/DiaryApiContext';
 // 다이어리 조각 작은거
 export default function DiaryFractal({ date, tripId, isValid, dailyInfo }) {
   const navigate = useNavigate();
+  console.log(dailyInfo);
 
   return (
     <li
@@ -19,7 +20,7 @@ export default function DiaryFractal({ date, tripId, isValid, dailyInfo }) {
     >
       <div className="tripDate">{date}</div>
       {/* 지출 내역 있으면, 즉 보여줄거면 사진 크기 작게 하고 밑에 지출내역 보여주고, 아니면 이미지 크게 보여줄 것 */}
-      {dailyInfo.imageUrl && <img src="" alt="thumbnail" />}
+      {dailyInfo.imageUrl !== undefined ? <img src="" alt="thumbnail" /> : ''}
     </li>
   );
 }
