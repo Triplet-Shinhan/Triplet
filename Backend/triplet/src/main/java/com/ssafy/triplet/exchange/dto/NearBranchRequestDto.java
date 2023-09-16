57a1d98bcf58a435a93109a9273f4bf5d02e7633
fangdol888 <fangdol888@gmail.com>
2023-09-16 14:29:49 +0900

    fix : requestbody에서 requestParam으로 받기로 변경

--- Backend/triplet/src/main/java/com/ssafy/triplet/exchange/dto/NearBranchRequestDto.java
+++ Backend/triplet/src/main/java/com/ssafy/triplet/exchange/dto/NearBranchRequestDto.java
@@ -1,5 +1,6 @@
 package com.ssafy.triplet.exchange.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 /**
@@ -7,6 +8,7 @@ import lombok.Data;
  */
 
 @Data
+@AllArgsConstructor
 public class NearBranchRequestDto {
     private Double latitude;
     private Double longitude;
